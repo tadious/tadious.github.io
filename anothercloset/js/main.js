5832c0df343e5838e781b0d0c8072fef81d592d7
Tadious Bohwasi <tadious@gmail.com>
2020-08-04 16:33:19 +0200

    small fix

--- anothercloset/js/main.js
+++ anothercloset/js/main.js
@@ -1352,10 +1352,6 @@ $(document).ready(function () {
 
     $('#contact-us-button').on("click", function () {
         var error = ValidationContactForm();
-        console.log($('#name').val());
-            console.log($('#email').val());
-            console.log($('#subject').val());
-            console.log($('#comment').val());
         if (error) {
             Email.send({
                 Host : "mail.svenskadomaner.se",
@@ -1373,22 +1369,6 @@ $(document).ready(function () {
                 $("#success-contact-form").fadeIn("slow");
                 $('#success-contact-form').delay(4000).fadeOut("slow");
             });
-            /*$.ajax({
-                type: "POST",
-                url: "email-templates/contact.php",
-                data: $("#contact-form").serialize(),
-                success: function (result) {
-                    // Un-comment below code to redirect user to thank you page.
-                    //window.location.href="thank-you.html";
-
-                    $('input[type=text],textarea').each(function () {
-                        $(this).val('');
-                    })
-                    $("#success-contact-form").html(result);
-                    $("#success-contact-form").fadeIn("slow");
-                    $('#success-contact-form').delay(4000).fadeOut("slow");
-                }
-            });*/
         }
     });
     function ValidationContactForm() {
